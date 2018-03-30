b66700fd17ba4f78f98df0bdcf1665672b2948ce
apahomi <pahomiandrei07@gmail.com>
2018-03-30 19:45:47 +0300

    HI

---
+++ lesson4/Lesson4/src/lesson4/ShoppingCart.java
@@ -0,0 +1,14 @@
+package lesson4;
+
+public class ShoppingCart {
+
+	public static void main(String[] args) {
+		String custName = "Hellen";
+		String itemDesc = "dress";
+		String message = custName + " buy" + " a " + itemDesc + ".";
+		
+		System.out.println(message);
+
+	}
+
+}
